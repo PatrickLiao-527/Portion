1b9eae9180b4d954827418bfab631022ac3f0276
Zhipei Liao <patrickliao527@gmail.com>
2024-05-29 08:25:56 -0700

    -updated responsive designs

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/styles/Header.css';
 import profilePic from '../assets/icons/profilePic.png';
+import portionLogo from '../assets/icons/portion-Logo.png';
 
 const Header = () => (
   <div className="header-wrapper">
     <div className="logo-wrapper">
-      <div className="logo"></div>
+      <img src={portionLogo} alt="Portion Logo" className="logo" />
       <div className="titles-wrapper">
-        <div className="header-title">Portion</div>
         <div className="header-title">Super Healthy Restaurant</div>
       </div>
     </div>
     <div className="nav-links">
       <a href="/contact-us">Contact Us</a>
       <div className="user-wrapper">
-        <Link to="/profile" className="user-link">
+        <Link to="/my-profile" className="user-link">
           <img src={profilePic} alt="Profile" className="user-avatar" />
           <div className="user-name">Alex Smith</div>
         </Link>
