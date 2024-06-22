88d836fa7e44446c2583bbf4f399e045b24e88de
Zhipei Liao <patrickliao527@gmail.com>
2024-06-22 14:28:53 -0700

    - changing base name

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-// src/OwnerApp.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
@@ -23,30 +22,30 @@ import './App.css';
 const AppContent = () => {
   return (
     <Routes>
-      <Route path="/owner/login" element={<Login />} />
-      <Route path="/owner/signup" element={<Signup />} />
-      <Route path="/owner/email-verification" element={<EmailVerification />} />
-      <Route path="/owner/contact-us" element={<ContactUs />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/email-verification" element={<EmailVerification />} />
+      <Route path="/contact-us" element={<ContactUs />} />
       <Route element={<ProtectedRoute />}>
         <Route
-          path="/owner/*"
+          path="/"
           element={
             <>
               <Header />
-              <NotificationBar /> 
+              <NotificationBar />
               <main>
                 <Sidebar />
                 <div className="content">
                   <Routes>
-                    <Route path="/owner" element={<Dashboard />} />
-                    <Route path="/owner/dashboard" element={<Dashboard />} />
-                    <Route path="/owner/my-orders" element={<MyOrders />} />
-                    <Route path="/owner/menu-items" element={<MenuItems />} />
-                    <Route path="/owner/transactions" element={<Transactions />} />
-                    <Route path="/owner/my-profile" element={<MyProfile />} />
-                    <Route path="/owner/reports" element={<Reports />} />
-                    <Route path="/owner/settings" element={<Settings />} />
-                    <Route path="/owner/contact-us" element={<ContactUs />} />
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/my-orders" element={<MyOrders />} />
+                    <Route path="/menu-items" element={<MenuItems />} />
+                    <Route path="/transactions" element={<Transactions />} />
+                    <Route path="/my-profile" element={<MyProfile />} />
+                    <Route path="/reports" element={<Reports />} />
+                    <Route path="/settings" element={<Settings />} />
+                    <Route path="/contact-us" element={<ContactUs />} />
                   </Routes>
                 </div>
               </main>
@@ -62,7 +61,7 @@ const App = () => {
   return (
     <AuthProvider>
       <WebSocketProvider>
-        <Router>
+        <Router basename="/owner">
           <div className="App">
             <AppContent />
           </div>
